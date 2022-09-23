80a1c85bdc45d41f185d994440ee544e6389565a
PPurington77 <papurington@gmail.com>
2022-09-23 13:49:53 -0600

    Updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
-
+import { Link, Navigate, Route, Routes } from 'react-router-dom';
+import Home from './views/Home';
 
 function App() {
   return (
     <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" replace />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
     </div>
   );
 }
