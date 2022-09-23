409bf174482905a3f4c31a09dce7a5130514c9dc
PPurington77 <papurington@gmail.com>
2022-09-23 13:41:04 -0600

    Internal API Service created

--- client/src/services/internalApiService.js
+++ client/src/services/internalApiService.js
@@ -1,5 +1,31 @@
 import axios from 'axios';
 
 const http = axios.create({
-  baseURL: 'http://localhost:8000/api',
+    baseURL: 'http://localhost:8000/api',
 });
+
+export const createBudget = async (data) => {
+    const res = await http.post('/budgets', data);
+    return res.data;
+};
+
+export const getAllBudgets = async (data) => {
+    const res = await http.get('/budgets');
+    return res.data;
+};
+
+export const getBudgetById = async (id) => {
+    const res = await http.get(`/budgets/${ id }`);
+    return res.data;
+};
+
+export const updateBudgetById = async (id, data) => {
+    const res = await http.put(`/budgets/${ id }`, data);
+    return res.data;
+};
+
+export const deleteBudgetById = async (id) => {
+    const res = await http.delete(`/budgets/${ id }`);
+    return res.data
+};
+
