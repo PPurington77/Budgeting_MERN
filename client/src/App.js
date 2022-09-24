e8a6a27f9c0699192cfeab5ed63f3242f342020c
PPurington77 <papurington@gmail.com>
2022-09-24 14:33:16 -0600

    Add budget route added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import { Link, Navigate, Route, Routes } from 'react-router-dom';
 import Home from './views/Home';
+import AddBudget from './views/AddBudget';
 
 function App() {
   return (
     <div>
         <Routes>
-          <Route path="/" element={<Navigate to="/home" replace />} />
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={ <Navigate to="/home" replace />} />
+          <Route path="/home" element={ <Home />} />
+          <Route path='/budgets/create' element={ <AddBudget />} />
         </Routes>
     </div>
   );
