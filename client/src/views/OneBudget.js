1cf0508e281c2558869a0d75f54e432cd6dee9bf
PPurington77 <papurington@gmail.com>
2022-09-29 15:25:52 -0400

    added ability to add an expense

--- client/src/views/OneBudget.js
+++ client/src/views/OneBudget.js
@@ -1,11 +1,17 @@
 import { useEffect, useState } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, useNavigate } from "react-router-dom";
 import { getBudgetById } from "../services/internalApiService";
 
 export const OneBudget = (props) => {
     const [budget, setBudget] = useState(null);
     const { id } = useParams();
+    const navigate = useNavigate();
+    //for expense
+    const [name, setName] = useState('');
+    const [category, setCategory] = useState('');
+    const [cost, setCost] = useState('');
 
+    //to get specific budget
     useEffect(() => {
         getBudgetById(id)
             .then((data) => {
@@ -22,17 +28,31 @@ export const OneBudget = (props) => {
     const { amount, expenses } = budget;
     let total = 0;
 
+    //creating new expense
+    const createExpense = (e) => {
+        e.preventDefault();
+
+        const newExpense = {
+            name,
+            category,
+            cost
+        };
+
+        const budgetExpenses = expenses;
+
+        budgetExpenses.push(newExpense);
+        navigate(`/budgets/${ id }`);
+    }
+
     return(
         <div className="w-50 p-4 rounded mx-auto shadow m-5">
             <h1 className="text-center mb-4">{ budget.name } Budget</h1>
             <div className="d-flex justify-content-center shadow align-items-center" style={{ backgroundColor: 'lightblue', borderRadius: '10px' }}>
-                <Link to={'/home'} className="btn btn-md btn-outline-primary mt-2">Home</Link>
                 <div className="card bg-light mb-3 m-3">
                     <div className="card-body">
                         <h5 className="card-title">Budget: ${ amount }</h5>
                     </div>
                 </div>
-                <Link to={'/create/expense'} className="btn btn-md btn-outline-primary mt-2">New Expense</Link>
             </div>
             <div>
                 <h2 className="text-center m-4">Your current expenses:</h2>
@@ -48,7 +68,38 @@ export const OneBudget = (props) => {
                     )
                 })}
             </div>
-
+            <form onSubmit={ (e) => createExpense(e) }>
+                <div className="form-group">
+                    <label className="h6">Expense Name:</label>
+                    <input onChange={ (e) => {
+                        setName(e.target.value);
+                    }} type='text' />
+                </div>
+                <div className="form-group">
+                    <label className="h6">Expense Category:</label>
+                    <select onChange={ (e) => {
+                        setCategory(e.target.value);
+                    }}>
+                        <option value='null'>Please Select Category</option>
+                        <option value='housing'>Housing</option>
+                        <option value='food'>Food</option>
+                        <option value='transportation'>Transportation</option>
+                        <option value='entertainment'>Entertainment</option>
+                        <option value='other'>Other</option>
+                    </select>
+                </div>
+                <div className="form-group">
+                    <label className="h6">Expense Cost:</label>
+                    <input onChange={ (e) => {
+                        setCost(e.target.value);
+                    }} type='number' />
+                </div>
+                <div className="mt-4">
+                    <button className="btn btn-md btn-outline-success">Create</button>
+                </div>
+            </form>
         </div>
     )
 }
+
+export default OneBudget;
