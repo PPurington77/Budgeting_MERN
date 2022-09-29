140b656cbaea571a94409324a5a59863796c6bbe
PPurington77 <papurington@gmail.com>
2022-09-29 14:47:46 -0400

    updated

--- client/src/views/OneBudget.js
+++ client/src/views/OneBudget.js
@@ -1,11 +1,10 @@
 import { useEffect, useState } from "react";
-import { useNavigate, useParams, Link } from "react-router-dom";
-import { getBudgetById, deleteBudgetById } from "../services/internalApiService";
+import { useParams, Link } from "react-router-dom";
+import { getBudgetById } from "../services/internalApiService";
 
 export const OneBudget = (props) => {
     const [budget, setBudget] = useState(null);
     const { id } = useParams();
-    const navigate = useNavigate();
 
     useEffect(() => {
         getBudgetById(id)
@@ -20,18 +19,20 @@ export const OneBudget = (props) => {
 
     if (budget === null ) return null;
 
-    const { name, amount, expenses } = budget;
+    const { amount, expenses } = budget;
     let total = 0;
 
     return(
         <div className="w-50 p-4 rounded mx-auto shadow m-5">
-            <h1 className="text-center mb-4">{ name } Budget</h1>
-            <div className="d-flex justify-content-center shadow" style={{ backgroundColor: 'lightblue', borderRadius: '10px' }}>
+            <h1 className="text-center mb-4">{ budget.name } Budget</h1>
+            <div className="d-flex justify-content-center shadow align-items-center" style={{ backgroundColor: 'lightblue', borderRadius: '10px' }}>
+                <Link to={'/home'} className="btn btn-md btn-outline-primary mt-2">Home</Link>
                 <div className="card bg-light mb-3 m-3">
                     <div className="card-body">
                         <h5 className="card-title">Budget: ${ amount }</h5>
                     </div>
                 </div>
+                <Link to={'/create/expense'} className="btn btn-md btn-outline-primary mt-2">New Expense</Link>
             </div>
             <div>
                 <h2 className="text-center m-4">Your current expenses:</h2>
@@ -47,6 +48,7 @@ export const OneBudget = (props) => {
                     )
                 })}
             </div>
+
         </div>
     )
 }
